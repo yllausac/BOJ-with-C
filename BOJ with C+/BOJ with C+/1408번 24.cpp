bf7d91bb43436a04f76b7640485f7ef8cf922dbf
yllausac <dadanana0805@gmail.com>
2021-08-09 21:32:50 +0900

    BOJ

---
+++ BOJ with C+/BOJ with C+/1408번 24.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+struct Time {
+	int h;
+	int m;
+	int s;
+};
+
+int findSum(Time t);
+
+int main() {
+	Time now, mission;
+	scanf_s("%d:%d:%d", &now.h, &now.m, &now.s);
+	scanf_s("%d:%d:%d", &mission.h, &mission.m, &mission.s);
+
+	int nowSum = findSum(now);
+	int misSum = findSum(mission);
+
+	int temp = misSum - nowSum;
+
+	if (nowSum < misSum) {
+		printf("%02d:%02d:%02d", (temp) / 3600, (temp) % 3600 / 60, (temp) % 60);
+
+	}
+	else {
+		temp = 24 * 3600 - nowSum + misSum;
+		printf("%02d:%02d:%02d", (temp) / 3600, (temp) % 3600 / 60, (temp) % 60);
+
+	}
+	return 0;
+}
+
+int findSum(Time t) {
+	int sum = t.h * 3600 + t.m * 60 + t.s;
+	return sum;
+}
