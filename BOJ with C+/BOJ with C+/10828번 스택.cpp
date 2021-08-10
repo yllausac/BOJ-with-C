dc86696f8be5c34d08c4a6af55abcd25adaed07a
yllausac <dadanana0805@gmail.com>
2021-08-10 17:57:51 +0900

    BOJ

--- BOJ with C+/BOJ with C+/10828번 스택.cpp
+++ BOJ with C+/BOJ with C+/10828번 스택.cpp
@@ -3,50 +3,63 @@
 
 using namespace std;
 
-int stack[10001], top = -1;
+int stack[10001], s;
 
 void push(int x) {
-	stack[++top] = x;
+	stack[s] = x;
+	s++;
 }
+
+int pop() {
+	if (s == 1)
+		return -1;
+	else
+		return stack[s - 1];
+		s--;
+}
+
+int size(){
+	return s;
+}
+
+int top() {
+	if (s == 0)
+		return -1;
+	else
+		return stack[s - 1];
+}
+
 int empty() {
-	if (top < 0) return 1;
-	else return 0;
-}
-void pop() {
-	if (empty() == 1)
-		cout << "-1" << endl;
-	else {
-		cout << stack[top] << endl;
-		stack[top--] = 0;
-	}
+	if (s == 0)
+		return 1;
+	else
+		return 0;
 }
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		char str[10];
+	while (n--) {
+		string str;
 		cin >> str;
-		if (!strcmp(str, "push")) {
-			int x;
-			cin >> x;
-			push(x);
+		if (str == "push") {
+			int n;
+			cin >> n;
+			push(n);
 		}
-		else if (!strcmp(str, "top")) {
-			if (empty() == 1)
-				cout << "-1" << endl;
-			else cout << stack[top] << endl;
+		if (str == "pop") {
+			cout << pop() << endl;
 		}
-		else if (!strcmp(str, "pop")) {
-			pop();
+		if (str == "top") {
+			cout << top() << endl;
 		}
-		else if (!strcmp(str, "empty")) {
+		if (str == "size") {
+			cout << size() << endl;
+		}
+		if (str == "empty") {
 			cout << empty() << endl;
 		}
-		else {
-			cout << top + 1 << endl;
-		}
 	}
-	return 0;
-
 }
