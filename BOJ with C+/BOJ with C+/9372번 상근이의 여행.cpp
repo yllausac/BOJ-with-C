7ffaaa117631c5e1a999af4687cb2cdad150454a
yllausac <dadanana0805@gmail.com>
2021-08-14 17:36:26 +0900

    BOJ

---
+++ BOJ with C+/BOJ with C+/9372번 상근이의 여행.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n, m;
+		cin >> n >> m;
+
+		int a, b;
+		while (m--) {
+			cin >> a >> b;
+		}
+		cout << n - 1 << endl;
+	}
+}
